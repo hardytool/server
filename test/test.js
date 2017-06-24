8d671b03fa6916966133941446e2d8814f1a78f0
Dan Ambrisco <dambrisco@gmail.com>
2017-06-23 20:28:29 -0600

    Add test TODO

---
+++ test/test.js
@@ -0,0 +1,8 @@
+console.log(`
+Currently, there are no tests for this project.
+
+This isn't ideal by any means, so to fix this we need to run the server under
+docker and use credentials stored within Travis to authenticate.
+
+Admin functionality *will not* be checked in this manner.
+`)
