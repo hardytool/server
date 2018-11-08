9dd7571a10fce59038e355da0db4b7bfd1a59fb6
kabobrocks <kabobrocks@gmail.com>
2018-11-08 16:45:30 -0600

    another break

--- src/pages/teams.js
+++ src/pages/teams.js
@@ -1,7 +1,7 @@
 var shortid = require("shortid")
 
 function list(templates, season, division, team, req, res) {
-  const season_id = req.params.season_id
+  var season_id = req.params.season_id
   const division_id = req.params.division_id
 
   season.getSeason(season_id).then(season => {
