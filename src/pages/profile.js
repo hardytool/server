b73434d76193bf304f05ad2a4d5bd03cd65ce2f0
kabobrocks <kabobrocks@gmail.com>
2019-12-13 18:55:21 -0600

    woops

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -42,7 +42,7 @@ function view(
                         })
                       }).then(({ is_vouched, voucher, teamsPlayed }) => {
                         const description = `RD2L Player ${_profile.name}
-                        Adjusted MMR: ${_profile.rank}
+                        Current Rank: ${_profile.rank}
                         Played on team(s): ${teamsPlayed.map(x=>x.name).join(', ')}.`
                         const html = templates.profile.view({
                           description: description,
