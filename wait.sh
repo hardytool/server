eec9e4523707ef267d1a41def197fbff6a307aa1
DJ Ambrisco <dambrisco@gmail.com>
2024-02-05 12:35:48 -0700

    Update wait.sh to not look for redis

--- wait.sh
+++ wait.sh
@@ -6,10 +6,10 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -c '\l' && redis-cli -h "redis"; do
-  >&2 echo "Postgres and redis are unavailable - sleeping"
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres and redis are up - executing command"
+>&2 echo "Postgres is up - executing command"
 exec $cmd
